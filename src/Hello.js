cc3dd77a87d83f849391cd7e5f5810b9a6cec925
Hiroko Yamaji <hiroko@hirokoymj.com>
2022-04-21 12:16:52 +0900

    Cleaned directory

--- src/Hello.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import { useCountRenders } from "./useCountRenders";
-
-export const Hello = React.memo(({ increment }) => {
-  // useCountRenders();
-
-  return <button onClick={() => increment(5)}>hello</button>;
-});
